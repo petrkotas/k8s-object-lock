8497fc24baf4379e2413f3e9560923d767fa74e1
Petr Kotas <petr.kotas@gmail.com>
2019-09-03 16:25:20 +0200

    Add Lock refined control

--- pkg/api/lock/v1/types.go
+++ pkg/api/lock/v1/types.go
@@ -11,6 +11,9 @@ import (
 type Lock struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LockSpec   `json:"spec,omitempty"`
+	Status LockStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -21,4 +24,18 @@ type LockList struct {
 	metav1.ListMeta `json:"metadata"`
 
 	Items []Lock `json:"items"`
+}
+
+// LockSpec defines what operations are locked for an object
+type LockSpec struct {
+	// Operations which are permitted on the object, when empty blocks all CRUD
+	Operations []string `json:"operations,omitempty"`
+	// SubResources which are permitted in the object, when empty block all
+	SubResources []string `json:"subresources,omitempty"`
+	// The message that will be returnes as a reason for locking the object
+	Reason string `json:"reason,omitempty"`
+}
+
+// LockStatus reflect current state of the lockr
+type LockStatus struct {
 }
